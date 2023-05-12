188f9fed9cebdd6dde3aa3f17f407fa5fdfe2528
Lukasz Kallas <lukasz.kallas@gmail.com>
2023-05-12 19:50:37 +0200

    Add repository functions and tests

--- repositories/question.js
+++ repositories/question.js
@@ -1,4 +1,5 @@
-const { readFile } = require('fs/promises')
+const { readFile, writeFile } = require('fs/promises')
+const { v4 } = require('uuid')
 
 const makeQuestionRepository = fileName => {
   const getQuestions = async () => {
@@ -8,11 +9,52 @@ const makeQuestionRepository = fileName => {
     return questions
   }
 
-  const getQuestionById = async questionId => {}
-  const addQuestion = async question => {}
-  const getAnswers = async questionId => {}
-  const getAnswer = async (questionId, answerId) => {}
-  const addAnswer = async (questionId, answer) => {}
+  const getQuestionById = async questionId => {
+    const questions = await getQuestions()
+    const question = questions.find(q => q.id === questionId)
+
+    return question
+  }
+
+  const addQuestion = async question => {
+    const questions = await getQuestions()
+    const newQuestion = { ...question, id: v4() }
+    questions.push(newQuestion)
+    const newQuestions = JSON.stringify(questions, null, 2)
+    const response = await writeFile(fileName, newQuestions)
+
+    return newQuestion
+  }
+
+  const getAnswers = async questionId => {
+    const question = await getQuestionById(questionId)
+    const answers = question.answers
+
+    return answers
+  }
+  const getAnswer = async (questionId, answerId) => {
+    const answers = await getAnswers(questionId)
+    const answer = answers.find(a => a.id === answerId)
+
+    return answer
+  }
+  const addAnswer = async (questionId, answer) => {
+    const questions = await getQuestions()
+    const question = await getQuestionById(questionId)
+    const newAnswer = { ...answer, id: v4() }
+    question.answers.push(newAnswer)
+    const updatedQuestions = questions.map(q => {
+      if (q.id === questionId) {
+        return question
+      }
+      return q
+    })
+
+    const newQuestions = JSON.stringify(updatedQuestions, null, 2)
+    await writeFile(fileName, newQuestions)
+
+    return newAnswer
+  }
 
   return {
     getQuestions,
--- repositories/question.test.js
+++ repositories/question.test.js
@@ -2,6 +2,21 @@ const { writeFile, rm } = require('fs/promises')
 const { faker } = require('@faker-js/faker')
 const { makeQuestionRepository } = require('./question')
 
+const testQuestions = [
+  {
+    id: faker.datatype.uuid(),
+    summary: 'What is my name?',
+    author: 'Jack London',
+    answers: []
+  },
+  {
+    id: faker.datatype.uuid(),
+    summary: 'Who are you?',
+    author: 'Tim Doods',
+    answers: []
+  }
+]
+
 describe('question repository', () => {
   const TEST_QUESTIONS_FILE_PATH = 'test-questions.json'
   let questionRepo
@@ -16,28 +31,83 @@ describe('question repository', () => {
     await rm(TEST_QUESTIONS_FILE_PATH)
   })
 
-  test('should return a list of 0 questions', async () => {
-    expect(await questionRepo.getQuestions()).toHaveLength(0)
+  describe('getQuestions', () => {
+    test('should return a list of 0 questions', async () => {
+      expect(await questionRepo.getQuestions()).toHaveLength(0)
+    })
+
+    test('should return a list of 2 questions', async () => {
+      await writeFile(TEST_QUESTIONS_FILE_PATH, JSON.stringify(testQuestions))
+
+      expect(await questionRepo.getQuestions()).toHaveLength(2)
+    })
   })
 
-  test('should return a list of 2 questions', async () => {
-    const testQuestions = [
-      {
-        id: faker.datatype.uuid(),
-        summary: 'What is my name?',
-        author: 'Jack London',
-        answers: []
-      },
-      {
-        id: faker.datatype.uuid(),
-        summary: 'Who are you?',
-        author: 'Tim Doods',
+  describe('getQuestionById', () => {
+    test('should return a question', async () => {
+      const question = await questionRepo.getQuestionById(testQuestions[0].id)
+
+      expect(question).toEqual(testQuestions[0])
+    })
+  })
+
+  describe('addQuestion', () => {
+    test('should add a question', async () => {
+      const newQuestion = {
+        summary: 'What is the shape of the Earth?',
+        author: 'John Stockton',
         answers: []
       }
-    ]
 
-    await writeFile(TEST_QUESTIONS_FILE_PATH, JSON.stringify(testQuestions))
+      await questionRepo.addQuestion(newQuestion)
 
-    expect(await questionRepo.getQuestions()).toHaveLength(2)
+      const questions = await questionRepo.getQuestions()
+
+      expect(questions).toHaveLength(3)
+      expect(questions[2]).toEqual({
+        ...newQuestion,
+        id: expect.any(String)
+      })
+    })
+  })
+
+  describe('addAnswer', () => {
+    test('should add an answer', async () => {
+      const newAnswer = {
+        author: 'Brian McKenzie',
+        summary: 'The Earth is flat.'
+      }
+
+      const questions = await questionRepo.getQuestions()
+
+      await questionRepo.addAnswer(questions[2].id, newAnswer)
+
+      const updatedQuestions = await questionRepo.getQuestions()
+
+      expect(updatedQuestions[2].answers).toHaveLength(1)
+      expect(updatedQuestions[2].answers[0]).toEqual({
+        ...newAnswer,
+        id: expect.any(String)
+      })
+    })
+  })
+
+  describe('getAnswers', () => {
+    test('should return a list of 0 answers', async () => {
+      expect(await questionRepo.getAnswers(testQuestions[0].id)).toHaveLength(0)
+    })
+  })
+
+  describe('getAnswer', () => {
+    test('should return an answer', async () => {
+      const questions = await questionRepo.getQuestions()
+
+      const answer = await questionRepo.getAnswer(
+        questions[2].id,
+        questions[2].answers[0].id
+      )
+
+      expect(answer).toEqual(questions[2].answers[0])
+    })
   })
 })
